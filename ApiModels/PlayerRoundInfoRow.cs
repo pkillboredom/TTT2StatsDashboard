117f73c04fdc101a75d593615d3b24e2142e7be2
Luke Tomkus <luke@pkillboredom.com>
2023-03-27 21:53:29 -0400

    Add detailed info for each round on click.

---
+++ ApiModels/PlayerRoundInfoRow.cs
@@ -0,0 +1,17 @@
+﻿namespace TTT2StatsApi.Models
+{
+    public class PlayerRoundInfoRow
+    {
+        public long RoundId { get; set; }
+        public string SteamId { get; set; }
+        public string FriendlyName { get; set; }
+        public string PlayerRole { get; set; }
+        public double StartingKarma { get; set; }
+        public double EndingKarma { get; set; }
+        public bool PlayerDied { get; set; }
+        public string? DeathCause { get; set; }
+        public bool? AirborneDeath { get; set; }
+        public bool? HeadshotDeath { get; set; }
+        public long? DeathTime { get; set; }
+    }
+}
