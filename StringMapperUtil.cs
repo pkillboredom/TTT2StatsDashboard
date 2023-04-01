9cc9b5ce1887dde25f61a82a1a4fb418ec80f1ea
Luke Tomkus <luke@pkillboredom.com>
2023-04-01 11:45:52 -0400

    Presentation Improvements Waste less space on datetime strings Convert 0,1 roles to strings. Round doubles to two decimal places @ RoundInfoTable and CombatLog

---
+++ StringMapperUtil.cs
@@ -0,0 +1,21 @@
+﻿namespace TTT2StatsDashboard
+{
+    public static class StringMapperUtil
+    {
+        public static string ToRoleFriendlyName(this string str)
+        {
+            if (str != null)
+            {
+                if (str == "0") return "Innocent";
+                if (str == "1") return "Traitor";
+            }
+            return str;
+        }
+
+        // Rounds a double to two decimal places returning as string
+        public static string ToRoundedString(this double d)
+        {
+            return Math.Round(d, 2).ToString();
+        }
+    }
+}
